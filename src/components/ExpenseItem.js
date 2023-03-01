179c68b9fafd8abbf8ae4c370322ce59326b436c
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-01 23:24:39 +0530

    Done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,13 +6,13 @@ function ExpenseItem() {
           <h1>ExpenseItems</h1>
         </header>
         <h2>
-          <pre>Food Rs 10</pre>
+          Food Rs 10
         </h2>
         <h2>
-          <pre>Petrol Rs 100</pre>
+          Petrol Rs 100
         </h2>
         <h2>
-          <pre>Movies Rs 200</pre>
+          Movies Rs 200
         </h2>
       </body>
     </div>
