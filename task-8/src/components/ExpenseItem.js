e188cc8e8e16edbdafe7e087d49a98d78a2c0b4c
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-03 17:49:07 +0530

    Done

--- task-8/src/components/ExpenseItem.js
+++ task-8/src/components/ExpenseItem.js
@@ -1,20 +1,19 @@
-import "./ExpenseIteam.css";
+import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from "./Card";
 
-function ExpenseIteam(props) {
-//   const d= new Date()
-//   const expensetitle = "Car insurance"
-//   const amount = 'RS 2000'
-//   const location = "delhi"
-    return (
-    <div className="container">
-      <h2>{props.date.toDateString()}</h2>
-      <div style={{display:"flex",alignItems:"center",gap:"20px"}}>
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <h2>{props.amount}</h2>
-        <h2>{props.location}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
-export default ExpenseIteam;
+export default ExpenseItem;
+
+
