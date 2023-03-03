b738d634d9331988fab2820167f55c04072f6cf3
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-03 19:05:08 +0530

    Done

--- task-8/src/components/Expenses/ExpenseItem.jsx
+++ task-8/src/components/Expenses/ExpenseItem.jsx
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const handleCLick=(e)=>{
+    e.target.parentNode.remove()
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
+      <button onClick={handleCLick}>Delete</button>
     </Card>
   );
 }
