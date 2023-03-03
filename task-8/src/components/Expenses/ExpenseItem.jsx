24960927c5b9c1c769c416c77a82aba6baec787e
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-03 19:42:15 +0530

    Done

--- task-8/src/components/Expenses/ExpenseItem.jsx
+++ task-8/src/components/Expenses/ExpenseItem.jsx
@@ -1,19 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [edit,setEdit]=useState(props.amount) 
   const handleCLick=(e)=>{
     e.target.parentNode.remove()
   }
+  const handleEdit=()=>{
+    setEdit("$100")
+  }
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />cd
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>{edit}</div>
       </div>
       <button onClick={handleCLick}>Delete</button>
+      <button onClick={handleEdit}>Edit</button>
     </Card>
   );
 }
