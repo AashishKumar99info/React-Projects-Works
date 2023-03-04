bf1678004f7eeae0e06277b0b0b80bd0b603a631
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-04 19:12:49 +0530

    Done

--- task-8/src/components/NewExpense/ExpenseForm.js
+++ task-8/src/components/NewExpense/ExpenseForm.js
@@ -1,40 +1,54 @@
 import "./ExpenseFrom.css"
+import {useState} from "react";
 const ExpenseForm = () => {
     
-    const handleTitle=(e)=>{
-        console.log(e.target.value)
+    
+const [enteredTitle,setEnteredTitle] = useState("")
+    
+        const titleChangeHandler=(e)=>{
+        setEnteredTitle(e.target.value)
     }
 
-    const handleAmount=(e)=>{
-        console.log(e.target.value)
+const [enteredAmount,setEnteredAmount] = useState("")
+
+    const amountChangeHandler=(e)=>{
+        setEnteredAmount(e.target.value)
     }
 
-    const handleDate=(e)=>{
-        console.log(e.target.value)
-    }
+const [enteredDate,setEnteredDate] = useState("")
 
-    const handleSubmisiion=(e)=>{
-        console.log(e.target.value)
-    }
+    const dateChangeHandler=(e)=>{
+        setEnteredDate(e.target.value)
+    } 
 
+    const handleSubmission = (e) => {
+        e.preventDefault();
+        const ExpenseDate={
+            Title: enteredTitle,
+            Amount: enteredAmount,
+            Date: new Date(enteredDate)
+        }
+        console.log(ExpenseDate)
+    }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmission}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input onChange={handleTitle} type = "text" placeholder="Enter the Title of your Expense"></input>
+                    <input onChange={titleChangeHandler} type = "text" placeholder="Enter the Title of your Expense"></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input onChange={handleAmount} type = "number" min = "0.01" step= "0.01" placeholder="Enter the Amount of Expense"></input>
+                    <input onChange={amountChangeHandler} type = "number" min = "0.01" step= "0.01" placeholder="Enter the Amount of Expense"></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input onChange={handleDate} type = "date" min="2019-01-01" max="2022-12-31" placeholder="Pick the date of Expense"></input>
+                    <input onChange={dateChangeHandler} type = "date" min="2019-01-01" max="2022-12-31" placeholder="Pick the date of Expense"></input>
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button onChange={handleSubmisiion} type = "submit">Add Expense</button>
+                <button type = "submit">Add Expense</button>
             </div>
         </form>
     )
